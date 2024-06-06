963b8dee28c14001b32fd87c4ac6a448ea970c54
Menno Tempelaar <menno@made-foryou.nl>
2024-06-06 10:15:44 +0200

    :sparkles: Add route caching and update observers
    This commit introduces a new RefreshRouteCacheAction that handles the cache refresh workflow for routes. Additionally, the RouteObserver and WithRouteObserver have been updated to utilize this new action during their save operations. The RouteResource has also been updated to include a header action allowing for manually triggering the cache refresh.

--- src/Observers/RouteObserver.php
+++ src/Observers/RouteObserver.php
@@ -2,23 +2,19 @@
 
 namespace MadeForYou\Routes\Observers;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
+use MadeForYou\Routes\Actions\RefreshRouteCacheAction;
 use MadeForYou\Routes\Models\Route;
 
 class RouteObserver
 {
+    /**
+     * @throws BindingResolutionException
+     */
     public function saved(Route $route): void
     {
-        Cache::forget(self::cacheKey());
-
-        Cache::rememberForever(self::cacheKey(), function () {
-            return Route::all();
-        });
-    }
-
-    public static function cacheKey(): string
-    {
-        return Config::get('routes.cache_key');
+        RefreshRouteCacheAction::run();
     }
 }
